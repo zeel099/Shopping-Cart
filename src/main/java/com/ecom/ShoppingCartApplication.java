a687fbcc348de1bfc13085a7214460b6258a08e7
Zeel sojitra <zeelmsojitra99@gmail.com>
2024-11-05 23:26:15 +0530

    shopping-cart

--- src/main/java/com/ecom/ShoppingCartApplication.java
+++ src/main/java/com/ecom/ShoppingCartApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShoppingCartApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Starting Application...");
+		System.out.println("Starting Application 123...");
 		SpringApplication.run(ShoppingCartApplication.class, args);
 	}
 
