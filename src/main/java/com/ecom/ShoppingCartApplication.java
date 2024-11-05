22e62ff52e65f7f7500292b715f42ff03127bb60
Zeel sojitra <zeelmsojitra99@gmail.com>
2024-11-05 22:43:39 +0530

    Adding loggers

--- src/main/java/com/ecom/ShoppingCartApplication.java
+++ src/main/java/com/ecom/ShoppingCartApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShoppingCartApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Starting Application...");
 		SpringApplication.run(ShoppingCartApplication.class, args);
 	}
 
